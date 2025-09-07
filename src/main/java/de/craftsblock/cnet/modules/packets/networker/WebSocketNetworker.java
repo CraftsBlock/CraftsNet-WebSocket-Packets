fbf3ada363b7a3ef171c584abc18e2c718d1ba60
Philipp <62473021+CrAfTsArMy@users.noreply.github.com>
2025-09-07 17:51:54 +0200

    docs: Fixed wrong author attribution

--- src/main/java/de/craftsblock/cnet/modules/packets/networker/WebSocketNetworker.java
+++ src/main/java/de/craftsblock/cnet/modules/packets/networker/WebSocketNetworker.java
@@ -21,12 +21,13 @@ import java.nio.ByteBuffer;
  * @param id               The unique identifier of this networker instance.
  * @param webSocketPackets The {@link WebSocketPackets} environment this networker belongs to.
  * @param webSocket        The underlying Java {@link WebSocket} connection.
+ * @author Philipp Maywald
  * @author CraftsBlock
  * @version 1.0.0
- * @since 1.0.0
  * @see Networker
  * @see Packet
  * @see WebSocket
+ * @since 1.0.0
  */
 public record WebSocketNetworker(long id, WebSocketPackets webSocketPackets, WebSocket webSocket) implements Networker {
 
